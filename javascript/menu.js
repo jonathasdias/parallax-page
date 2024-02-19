9940f64ee4f1fc6e351ab5446bc8f7b4b7c8315a
Jonathas Dias <jonathas.dev446@gmail.com>
2024-02-19 16:00:39 -0300

    Projeto completo

--- javascript/menu.js
+++ javascript/menu.js
@@ -2,7 +2,33 @@ const btn_menu = document.querySelector('.btn-menu');
 
 const menu = document.querySelector('.menu');
 
-btn_menu.addEventListener('click', function() {
-    this.classList.toggle('change');
-    menu.classList.toggle('menu-active');
+function openMenu() {
+    menu.style.display = 'flex';
+
+    setTimeout(() => {
+        btn_menu.classList.add('change');
+        menu.classList.add('menu-active');
+        btn_menu.ariaPressed = true;
+    }, 1);
+}
+
+function clearMenu() {
+
+    setTimeout(() => {
+        menu.style.display = 'none';
+    }, 300);
+
+    btn_menu.classList.remove('change');
+    menu.classList.remove('menu-active');
+    btn_menu.ariaPressed = false;
+}
+
+btn_menu.addEventListener('click', ()=> {
+    
+    if(btn_menu.classList.contains('change')) {
+        clearMenu();
+    }else {
+        openMenu();
+    }
+
 })
